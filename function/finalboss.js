96c19ce9a9aafa6f77df5c1cb66a238891de8444
myst <myst@grandchase>
2023-07-18 17:10:44 +0800

    70%

---
+++ function/finalboss.js
@@ -0,0 +1,176 @@
+let playerLives = 3;
+let bossLife = 3;
+const weapons = ["rock", "paper", "scissors"];
+let isFinalBoss = false;
+let playerWins = 0;
+let gameInProgress = false;
+const bossWeaponImage = document.querySelector(".boss-weapon");
+const choiceWeapons = document.querySelectorAll(".choice-weapon");
+const gameContainer = document.querySelector("#main-container");
+
+const getBossChoice = () => {
+  if (isFinalBoss) {
+    const finalBossWeapons = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * finalBossWeapons.length);
+    const bossChoice = finalBossWeapons[randomIndex];
+    // Update boss weapon image
+    bossWeaponImage.setAttribute("src", `assets/images/${bossChoice}.png`);
+    return bossChoice;
+  } else {
+    const botWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+    // Update boss weapon image
+    bossWeaponImage.setAttribute("src", `assets/images/${botWeapon}.png`);
+    return botWeapon;
+  }
+};
+const getPlayerChoice = () => {
+  choiceWeapons.forEach(choice => {
+    choice.addEventListener("click", () => {
+      if (gameInProgress) {
+        const playerChoice = choice.getAttribute("data-choice");
+        const bossChoice = getBossChoice();
+        playRound(playerChoice, bossChoice);
+        // Update the player's weapon image
+        const playerWeaponImage = document.querySelector("#playerWeapon");
+        const chosenWeaponImageSrc = choice.getAttribute("src");
+        playerWeaponImage.src = chosenWeaponImageSrc;
+      }
+    });
+  });
+};
+const increaseWins = () => {
+  playerWins++;
+};
+//Modals
+const openModalButton = document.getElementById('openModalButton');
+const closeModalButton = document.getElementById('closeModalButton');
+const modalOverlay = document.getElementById('modalOverlay');
+const modalTitle = document.getElementById('modalTitle');
+const modalMessage = document.getElementById('modalMessage');
+
+openModalButton.addEventListener('click', function() {
+  showModal("Draw", "You both used scissors");
+});
+
+closeModalButton.addEventListener('click', function() {
+  hideModal();
+});
+
+const showModal = (title, message, resultClass) => {
+  modalTitle.textContent = title;
+  modalMessage.textContent = message;
+  modalMessage.className = resultClass;
+  modalOverlay.style.display = 'flex';
+};
+
+const hideModal = () => {
+  modalOverlay.style.display = 'none';
+};
+
+const playRound = (playerChoice, bossChoice) => {
+  const success = `Impressive! You won! You used ${playerChoice}, and your opponent used ${bossChoice}`;
+  const failed = `You lost! You used ${playerChoice}, and your opponent used ${bossChoice}`;
+  const draw = `Draw! You both used ${playerChoice}`;
+  const success2 = `One more win to go!`;
+  const success3 = `Congratulations! You defeated the final boss!`;
+
+  switch (true) {
+    case playerChoice === bossChoice:
+      showModal("Draw", draw, "draw");
+      break;
+    case playerChoice === "scissors" && bossChoice === "paper":
+    case playerChoice === "rock" && bossChoice === "scissors":
+    case playerChoice === "paper" && bossChoice === "rock":
+      increaseWins();
+      BossdecreaseLives();
+      if(playerWins === 1){
+        showModal("Success", success, "success");
+      }else if(playerWins === 2){
+        showModal("Success", success2, "success");
+      }
+      else if(playerWins === 3){
+        showModal("Success", success3, "success");
+      }
+      break;
+    default:
+      showModal("Failed", failed, "failed");
+      decreaseLives();
+      break;
+  }
+};
+
+const startGame = () => {
+  if (playerLives > 0) {
+    gameInProgress = true;
+    if (gameContainer.style.display === "none") {
+      setTimeout(() => {
+        gameContainer.style.display = "block";
+        startContainer.style.display = "none";
+      }, 2000); // Delay of 2000 milliseconds (2 seconds)
+    }
+  } else {
+    console.log("GameOver")
+    // document.querySelector('.result').innerHTML = "Game Over!";
+    gameInProgress = false;
+  }
+
+  getPlayerChoice();
+  document.querySelector(".lives").innerHTML = "HP: " + playerLives;
+  document.querySelector(".bosslives").innerHTML = "HP: " + bossLife;
+};
+
+
+const decreaseLives = () => {
+  playerLives--;
+  document.querySelector(".lives").innerHTML = "HP: " + playerLives;
+  if (playerLives === 0) {
+    console.log("Game over! You have no more lives.");
+    showPlayAgainModal();
+  }
+};
+
+const BossdecreaseLives = () => {
+  bossLife--;
+  document.querySelector(".bosslives").innerHTML = "HP: " + bossLife;
+};
+const showPlayAgainPrompt = () => {
+  showPlayAgainModal();
+};
+
+const resetGame = () => {
+  window.location.assign("index.html");
+  playerLives = 3;
+  playerWins = 0;
+  isFinalBoss = false;
+  console.log("Game reset. Starting a new game.");
+  startGame();
+};
+const playAgainButton = document.getElementById('playAgainButton');
+const playAgainYesButton = document.getElementById('playAgainYes');
+const playAgainNoButton = document.getElementById('playAgainNo');
+const playAgainModal = document.getElementById('playAgainModal');
+
+playAgainButton.addEventListener('click', function() {
+  showPlayAgainPrompt();
+});
+
+playAgainYesButton.addEventListener('click', function() {
+  resetGame();
+  hidePlayAgainModal();
+});
+
+playAgainNoButton.addEventListener('click', function() {
+  resetGame();
+  hidePlayAgainModal();
+});
+
+const showPlayAgainModal = () => {
+  playAgainModal.style.display = 'flex';
+  
+};
+
+const hidePlayAgainModal = () => {
+  playAgainModal.style.display = 'none';
+};
+
+startGame();
--- function/index.js
+++ function/index.js
@@ -4,31 +4,39 @@ const weapons = ["rock", "paper", "scissors"];
 let isFinalBoss = false;
 let playerWins = 0;
 let gameInProgress = false;
-//loading screen
-// window.addEventListener('load', function() {
-//   var loadingScreen = document.getElementById('loading-screen');
-//   var content = document.getElementById('content');
-//   loadingScreen.style.display = 'none';
-// });
+const bossWeaponImage = document.querySelector(".boss-weapon");
+const choiceWeapons = document.querySelectorAll(".choice-weapon");
+const gameContainer = document.querySelector("#main-container");
+const startContainer = document.querySelector("#startcontainer");
+
+
+window.addEventListener('load', function() {
+  var loadingScreen = document.getElementById('loading-screen');
+  var content = document.getElementById('content');
+  loadingScreen.style.display = 'none';
+});
 const getBossChoice = () => {
   if (isFinalBoss) {
     const finalBossWeapons = ["rock", "paper", "scissors"];
-    return finalBossWeapons[Math.floor(Math.random() * finalBossWeapons.length)];
+    const randomIndex = Math.floor(Math.random() * finalBossWeapons.length);
+    const bossChoice = finalBossWeapons[randomIndex];
+    // Update boss weapon image
+    bossWeaponImage.setAttribute("src", `assets/images/${bossChoice}.png`);
+    return bossChoice;
   } else {
     const botWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+    // Update boss weapon image
+    bossWeaponImage.setAttribute("src", `assets/images/${botWeapon}.png`);
     return botWeapon;
   }
 };
-
 const getPlayerChoice = () => {
-  const choiceWeapons = document.querySelectorAll(".choice-weapon");
   choiceWeapons.forEach(choice => {
     choice.addEventListener("click", () => {
       if (gameInProgress) {
         const playerChoice = choice.getAttribute("data-choice");
         const bossChoice = getBossChoice();
         playRound(playerChoice, bossChoice);
-
         // Update the player's weapon image
         const playerWeaponImage = document.querySelector("#playerWeapon");
         const chosenWeaponImageSrc = choice.getAttribute("src");
@@ -37,45 +45,109 @@ const getPlayerChoice = () => {
     });
   });
 };
+const increaseWins = () => {
+  playerWins++;
+  const bossImage = document.querySelector(".boss");
+  console.log(`Player wins: ${playerWins}`);
+  if (playerWins === 1) {
+    console.log(`You defeated mini boss ${playerWins}! Get ready for the next one!`);
+    // Update boss image for the next mini boss
+    const bossImage = document.querySelector(".boss");
+    bossWeaponImage.src = `assets/images/questionmark.png`;
+    const bossImagePath = `assets/images/boss/serdin/mini2.png`;
+    bossImage.src = bossImagePath;
+  }
+  else if(playerWins === 2) {
+    console.log(`You defeated mini boss ${playerWins}! Get ready for the next one!`);
+    // Update boss image for the next mini boss
+    bossWeaponImage.src = `assets/images/questionmark.png`;
+    const bossImagePath = `assets/images/boss/serdin/mini3.webp`;
+    bossImage.src = bossImagePath;
+  }
+  else if(playerWins === 3) {
+    console.log(`You defeated mini boss ${playerWins}! Get ready for the next one!`);
+    // Update boss image for the next mini boss
+    bossWeaponImage.src = `assets/images/questionmark.png`;
+    const bossImagePath = `assets/images/boss/serdin/mini4.webp`;
+    bossImage.src = bossImagePath;
+  }
+  else if (playerWins === 4) {
+    document.body.classList.add('blur-effect');
+    setTimeout(() => {
+      window.location.assign("final_boss.html");
+    }, 5000);
+  } else if (playerWins === 5) {
+    console.log("Congratulations! You defeated the final boss!");
+    // Update boss image for the final boss
+    const bossImage = document.querySelector(".boss");
+    const bossImagePath = "assets/images/boss/serdin/finalboss.webp";
+    bossImage.src = bossImagePath;
+  }else{
+    showPlayAgainPrompt();
+  }
+};
+//Modals
+const openModalButton = document.getElementById('openModalButton');
+const closeModalButton = document.getElementById('closeModalButton');
+const modalOverlay = document.getElementById('modalOverlay');
+const modalTitle = document.getElementById('modalTitle');
+const modalMessage = document.getElementById('modalMessage');
 
+openModalButton.addEventListener('click', function() {
+  showModal("Draw", "You both used scissors");
+});
+
+closeModalButton.addEventListener('click', function() {
+  hideModal();
+});
+
+const showModal = (title, message, resultClass) => {
+  modalTitle.textContent = title;
+  modalMessage.textContent = message;
+  modalMessage.className = resultClass;
+  modalOverlay.style.display = 'flex';
+};
+
+const hideModal = () => {
+  modalOverlay.style.display = 'none';
+};
 
 const playRound = (playerChoice, bossChoice) => {
-  const success = `Impressive! You won! You used ${playerChoice} and your opponent used ${bossChoice}`;
-  const failed = `You lost! You used ${playerChoice} and your opponent used ${bossChoice}`;
+  const success = `Impressive! You won! You used ${playerChoice}, and your opponent used ${bossChoice}`;
+  const failed = `You lost! You used ${playerChoice}, and your opponent used ${bossChoice}`;
   const draw = `Draw! You both used ${playerChoice}`;
+  const success2 = `Impressive! You won! 1 more to go in final boss`;
+  const success3 = `You'll be directing final boss`;
 
   switch (true) {
     case playerChoice === bossChoice:
-      console.log(draw);
-      // document.querySelector('.result').innerHTML = draw;
+      showModal("Draw", draw, "draw");
       break;
     case playerChoice === "scissors" && bossChoice === "paper":
     case playerChoice === "rock" && bossChoice === "scissors":
     case playerChoice === "paper" && bossChoice === "rock":
       increaseWins();
-      console.log(success)
-      // document.querySelector('.result').innerHTML = success;
+      if(playerWins === 3){
+        showModal("Success", success2, "success");
+      }else if(playerWins <= 2){
+        showModal("Success", success, "success");
+      }
+      else if(playerWins === 4){
+        showModal("Success", success3, "success");
+      }
       break;
     default:
+      showModal("Failed", failed, "failed");
       decreaseLives();
-      console.log(failed);
-      // document.querySelector('.result').innerHTML = failed;
       break;
   }
 };
 
 const startGame = () => {
-  const gameContainer = document.querySelector("#main-container");
-  const startContainer = document.querySelector("#startcontainer");
-  // const loadingScreen = document.querySelector("#loading-screen");
-
   if (playerLives > 0) {
     gameInProgress = true;
     if (gameContainer.style.display === "none") {
-      // loadingScreen.style.display = "block";
-
       setTimeout(() => {
-        // loadingScreen.style.display = "none";
         gameContainer.style.display = "block";
         startContainer.style.display = "none";
       }, 2000); // Delay of 2000 milliseconds (2 seconds)
@@ -87,48 +159,60 @@ const startGame = () => {
   }
 
   getPlayerChoice();
+  document.querySelector(".lives").innerHTML = "HP: " + playerLives;
 };
 
 
 const decreaseLives = () => {
   playerLives--;
-  console.log(playerLives);
-  // document.querySelector(".lives").innerHTML = playerLives;
+  document.querySelector(".lives").innerHTML = "HP: " + playerLives;
   if (playerLives === 0) {
     console.log("Game over! You have no more lives.");
-    showPlayAgainPrompt();
+    showPlayAgainModal();
   }
 };
 
-const increaseWins = () => {
-  playerWins++;
-  console.log(`Player wins: ${playerWins}`);
-  if (playerWins === 5 && isFinalBoss) {
-    console.log("Congratulations! You defeated the final boss!");
-    showPlayAgainPrompt();
-  } else if (playerWins === 4) {
-    console.log("You defeated the regular bosses! Get ready for the final boss!");
-    isFinalBoss = true;
-  }
-};
+
 
 const showPlayAgainPrompt = () => {
-  const playAgain = confirm("Do you want to play again?");
-  if (playAgain) {
-    resetGame();
-    location.reload();
-  } else {
-    console.log("Thanks for playing!");
-    location.reload();
-  }
+  showPlayAgainModal();
 };
 
 const resetGame = () => {
+  location.reload();
   playerLives = 3;
   playerWins = 0;
   isFinalBoss = false;
   console.log("Game reset. Starting a new game.");
   startGame();
 };
+const playAgainButton = document.getElementById('playAgainButton');
+const playAgainYesButton = document.getElementById('playAgainYes');
+const playAgainNoButton = document.getElementById('playAgainNo');
+const playAgainModal = document.getElementById('playAgainModal');
+
+playAgainButton.addEventListener('click', function() {
+  showPlayAgainPrompt();
+});
+
+playAgainYesButton.addEventListener('click', function() {
+  resetGame();
+  hidePlayAgainModal();
+});
+
+playAgainNoButton.addEventListener('click', function() {
+  resetGame();
+  hidePlayAgainModal();
+});
+
+const showPlayAgainModal = () => {
+  playAgainModal.style.display = 'flex';
+  
+};
+
+const hidePlayAgainModal = () => {
+  playAgainModal.style.display = 'none';
+};
+
 
 
